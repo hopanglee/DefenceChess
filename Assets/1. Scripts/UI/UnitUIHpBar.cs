b09913f273dd4d68fcddc6c7b59a1f169fa08af5
hopanglee <hgknim87j@naver.com>
2025-01-13 16:05:40 +0900

    HP bar separator적용

--- Assets/1. Scripts/UI/UnitUIHpBar.cs
+++ Assets/1. Scripts/UI/UnitUIHpBar.cs
@@ -1,12 +1,25 @@
 using System;
-using NUnit.Framework;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class UnitUIHPBar : UIBehaviour, IUIView
 {
+    #region Separator Shader Field
+    private const string STEP = "_Steps";
+    private const string RATIO = "_HSRatio";
+    private const string WIDTH = "_Width";
+    private const string THICKNESS = "_Thickness";
+
+    private static readonly int floatSteps = Shader.PropertyToID(STEP);
+    private static readonly int floatRatio = Shader.PropertyToID(RATIO);
+    private static readonly int floatWidth = Shader.PropertyToID(WIDTH);
+    private static readonly int floatThickness = Shader.PropertyToID(THICKNESS);
+    #endregion
+
+
     [SerializeField]
     private Slider hpSlider;
 
@@ -17,6 +30,30 @@ public class UnitUIHPBar : UIBehaviour, IUIView
     private int m_curHp;
     [SerializeField]
     private int m_shieldAmount;
+
+    [SerializeField]
+    private Image separator;
+
+    private float steps = 10f;
+
+    //public float RectWidth = 100f;
+
+    [Range(0, 5f)]
+    [SerializeField]
+    private float Thickness = 2f;
+
+    [ContextMenu("Create Material")]
+    private void CreateMaterial()
+    {
+        {
+            separator.material = new Material(Shader.Find("UI/Health Separator"));
+        }
+    }
+
+    protected override void Awake()
+    {
+        CreateMaterial();
+    }
 
     public void HpUIUpdate(int value)
     {
@@ -38,8 +75,8 @@ public class UnitUIHPBar : UIBehaviour, IUIView
 
     public void UIUpdate()
     {
-        //float step;
-        float hpShieldRatio;
+        float step;
+        //float hpShieldRatio;
 
         // 쉴드가 존재 할 때
         if (m_shieldAmount > 0)
@@ -47,16 +84,18 @@ public class UnitUIHPBar : UIBehaviour, IUIView
             // 현재체력 + 쉴드 > 최대 체력
             if (m_curHp + m_shieldAmount > MaxHp)
             {
-                hpShieldRatio = (float)m_curHp / (m_curHp + m_shieldAmount);
+                //hpShieldRatio = (float)m_curHp / (m_curHp + m_shieldAmount);
                 shieldSlider.value = 1f;
-                //step = (m_curHp) / steps;
+                //step = m_curHp / steps;
+                step = (MaxHp + m_shieldAmount) / steps;
                 hpSlider.value = (float)m_curHp / (m_curHp + m_shieldAmount);
             }
             else
             {
                 shieldSlider.value = (float)(m_curHp + m_shieldAmount) / MaxHp;
-                hpShieldRatio = (float)m_curHp / MaxHp;
+                //hpShieldRatio = (float)m_curHp / MaxHp;
                 //step = m_curHp / steps;
+                step = (MaxHp + m_shieldAmount) / steps;
 
                 hpSlider.value = (float)m_curHp / MaxHp;
             }
@@ -66,18 +105,19 @@ public class UnitUIHPBar : UIBehaviour, IUIView
         else
         {
             shieldSlider.value = 0f;
-            //step = MaxHp / steps;
-            hpShieldRatio = 1f;
+            step = MaxHp / steps;
+            //hpShieldRatio = 1f;
 
             hpSlider.value = (float)m_curHp / MaxHp;
         }
 
         //         damaged.fillAmount = Mathf.Lerp(damaged.fillAmount, hp.fillAmount, Time.deltaTime * speed);
 
-        //         separator.material.SetFloat(floatSteps, step);
-        //         separator.material.SetFloat(floatRatio, hpShieldRatio);
-        //         separator.material.SetFloat(floatWidth, RectWidth);
-        //         separator.material.SetFloat(floatThickness, Thickness);
+        separator.material.SetFloat(floatSteps, step);
+        //separatorMat.SetFloat(floatRatio, hpShieldRatio);
+        separator.material.SetFloat(floatRatio, 1f);
+        //separatorMat.SetFloat(floatWidth, RectWidth);
+        separator.material.SetFloat(floatThickness, Thickness);
     }
 
     public void Show()
